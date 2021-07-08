bee9e488d2b7799d08e8a13f5ace97d91abf791b
Rohit Sharma <rohitsharma@MacBook-Pro-2.broadband>
2021-07-08 12:50:38 +0100

    giphys

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
-    render() {
-        const src = `https://media4.giphy.com/media/${this.props.id}/giphy.gif`
-        return (
-         <img src={src} alt="" className="gif"/>
-        );
-    }
+  render() {
+    const src = `https://media4.giphy.com/media/${this.props.id}/giphy.gif`;
+    return (
+      <img src={src} alt="" className="gif" />
+    );
+  }
 }
-
 export default Gif;
--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import Gif from './gif';
 
 class GifList extends Component {
-    renderList = () => {
-        {this.props.gifs.map(gif => {
-            return <Gif id = {gif.id} key={gif.id} />
-        })
-    }
-    render() {
-        return (
-            <div className="gif-list"> 
-                {this.renderList()};
-            </div>       
-        ); 
-    };
-};
+     renderList = () => {
+       return this.props.gifs.map((gif) => {
+         return <Gif id={gif.id} key={gif.id} />;
+       });
+     }
+
+     render() {
+       return (
+         <div className="gif-list">
+           {this.renderList()}
+         </div>
+       );
+     }
+}
 export default GifList;
